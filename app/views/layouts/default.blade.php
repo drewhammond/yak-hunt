bcb9955375f4626da33a2125cf7ffcf1d255c718
Drew Hammond <drew@alphagenetica.com>
2014-10-16 22:10:13 -0400

    Default blade template for global layouts

---
+++ app/views/layouts/default.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+<title>Yak Hunt</title>
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+</head>
+<body>
+
+<div class="container">
+
+</div>
+
+<div class="footer">
+    <p class="copyright muted">&copy; 2014 <a href="https://drewhammond.me">Drew Hammond</a></p>
+</div>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+</body>
+</html>
