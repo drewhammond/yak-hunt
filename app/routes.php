089664d2751957b4a69dd7856770f3f30ed36e23
Drew Hammond <drew@alphagenetica.com>
2014-11-23 20:17:04 -0500

    Default layout view

--- app/routes.php
+++ app/routes.php
@@ -12,3 +12,5 @@
 */
 
 Route::get('/', 'HomeController@index');
+
+// API routes @todo
--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -4,16 +4,26 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 <title>Yak Hunt</title>
-<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
 <link rel="stylesheet" href="assets/css/app.css">
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDGli0jH_sT_1PPKtEB98TdmeEcDHsqlXE"></script>
 </head>
 <body>
 
 @include('layouts.partials.navigation');
+<div class="row">
+<div class="container-fluid map-container">
 
-<div class="container">
+    <div class="col-sm-6">
+        <div id="map-canvas"></div>
+    </div>
+    <div class="col-sm-6">
+    Blah blah blah
+    </div>
+</div>
 
 </div>
+
 
 <div class="footer">
     <div class="container">
@@ -21,6 +31,9 @@
     </div>
 </div>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+<script src="{{ asset('assets/vendor/gmaps/gmaps.js') }}"></script>
+<script type="text/javascript" src="assets/js/app.js"></script>
+
 </body>
 </html>
